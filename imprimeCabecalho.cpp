9ae8c633f3f9d4b2c335911adaf648b869e01c09
Leonardo Possan <leonardopossamdasilva@gmail.com>
2023-03-01 19:24:11 -0300

    Fazendo pequenos ajustes

--- imprimeCabecalho.cpp
+++ imprimeCabecalho.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
+using namespace std;
+
 void imprimeCabecalho(){
     std::cout << "************************************** " << std::endl;
-    std::cout << "Seja bem vindo ao nosso jogo da forca. " << std::endl;
+    std::cout << "Seja bem vindo ao nosso jogo da Adivinhacao. " << std::endl;
     std::cout << "************************************** " << std::endl;
+
+    std::cout << endl;
 }
--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include "desejaJogar.h"
 #include "imprimeCabecalho.h"
+#include "nivelJogo.h"
+#include "numeroSecreto.h"
+
+using namespace std;
 
 int main(){
     desejaJogar();
     imprimeCabecalho();
+    nivelJogo();
+    numeroSecreto();
 
     return 0;
 }
